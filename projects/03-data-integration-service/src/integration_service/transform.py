255fd6a7eb1ae56c22c13ca5fa636c89c34f5dc3
JacksonFaller <jacksonfaller@protonmail.com>
2026-09-06 17:27:24 +0400

    Add integration transformation exercise scaffold

---
+++ projects/03-data-integration-service/src/integration_service/transform.py
@@ -0,0 +1,6 @@
+from .models import ExternalProduct, Product
+
+
+def transform_product(payload: dict) -> Product:
+    """TODO: validate/normalize one external product payload."""
+    raise NotImplementedError
